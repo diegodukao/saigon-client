40be3986201306da59d1875156c096c9f31b9bf0
Diego Dukão <diegodukao@gmail.com>
2016-10-18 16:51:42 -0200

    generating board buttons with python

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+
+
+class Board(GridLayout):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        for i in range(25):
+            button = Button(text="palavra")
+            self.add_widget(button)
 
 
 class SaigonClient(App):
