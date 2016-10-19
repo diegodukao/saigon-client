ae9ec143f11a94a824875cbe5577ab21a372a04d
Diego Dukão <diegodukao@gmail.com>
2016-10-19 03:35:51 -0200

    choosing a team for the user

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from kivy.app import App
+from kivy.properties import StringProperty
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.gridlayout import GridLayout
+
+from random import choice
+from uuid import uuid4
+
+TEAMS = [
+    "blue",
+    "red",
+]
 
 
 class Board(GridLayout):
@@ -14,7 +23,16 @@ class Board(GridLayout):
 
 
 class SaigonClient(App):
-    pass
+    player_id = StringProperty()
+    team = StringProperty()
+
+    def on_start(self):
+        self.player_id = str(uuid4())
+        self.team = self.choose_team()
+        self.root.board_screen.team_label.text = self.team
+
+    def choose_team(self):
+        return choice(TEAMS)
 
 
 if __name__ == "__main__":
